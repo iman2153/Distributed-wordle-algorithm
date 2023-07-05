bb014df5863efc1bc492703637ad0a05982f4eee
Iman Morshed <67348658+iman2153@users.noreply.github.com>
2023-07-04 19:22:49 -0700

    Delete general.h

--- general.h
+++
@@ -1,9 +0,0 @@
-#ifndef general_compiled
-#define general_compiled
-#include <string>
-#include <json/json.h>
-#include <vector>
-#include <iostream>
-#include <fstream>
-#include <algorithm>
-#endif
